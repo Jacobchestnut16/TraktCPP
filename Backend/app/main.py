e54eb1f0ee8f69b8c546f2fb3e6c418f2de665f7
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2025-10-02 22:01:20 -0400

    Authorized

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 from app.routes import auth, media, user, sync
 
 def create_app():
-    app = FastAPI(title="Trakt Backend", version="0.1")
+    app = FastAPI(title="Film Finder", version="0.1")
 
     # include routes
-    app.include_router(auth.router, prefix="/auth", tags=["auth"])
-    app.include_router(media.router, prefix="/media", tags=["media"])
-    app.include_router(sync.router, prefix="/sync", tags=["sync"])
+    app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+    app.include_router(media.router, prefix="/media", tags=["Media"])
+    app.include_router(sync.router, prefix="/sync", tags=["Sync"])
 
-    app.include_router(user.router, prefix="/user", tags=["user"])
+    app.include_router(user.router, prefix="/user", tags=["User"])
 
     return app
 
